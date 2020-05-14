7ba8f26e29cc9c8ba5bee042386ce32639afbbb9
Oscar Abrina <abrinao@amazon.com>
2020-05-14 00:30:07 -0700

    Achieve coverage on MQTT_SerializeConnect and serializeConnectPacket

--- libraries/standard/mqtt/utest/mqtt_utest.c
+++ libraries/standard/mqtt/utest/mqtt_utest.c
@@ -2,14 +2,26 @@
 
 #include "unity.h"
 
-#include "mock_mqtt_lightweight.h"
-
 /* Include paths for public enums, structures, and macros. */
+#include "mqtt_lightweight.h"
 #include "mqtt.h"
 
-#define MQTT_TEST_BUFFER_LENGTH    ( 1024 )
+/**
+ * @brief Size of the fixed and variable header of a CONNECT packet.
+ */
+#define MQTT_PACKET_CONNECT_HEADER_SIZE    ( 10UL )
 
-static uint8_t buffer[ MQTT_TEST_BUFFER_LENGTH ] = { 0 };
+#define MQTT_TEST_USERNAME                 "username"
+#define MQTT_TEST_USERNAME_LEN             sizeof( MQTT_TEST_USERNAME ) - 1
+
+#define MQTT_TEST_PASSWORD                 "password"
+#define MQTT_TEST_PASSWORD_LEN             sizeof( MQTT_TEST_PASSWORD ) - 1
+
+#define CLIENT_IDENTIFIER_LEN              sizeof( CLIENT_IDENTIFIER ) - 1
+
+#define MQTT_TEST_BUFFER_LENGTH            ( 1024 )
+
+static uint8_t mqttBuffer[ MQTT_TEST_BUFFER_LENGTH ] = { 0 };
 
 /* ============================   UNITY FIXTURES ============================ */
 void setUp( void )
@@ -55,9 +67,68 @@ void test_Mqtt_SerializeConnect_invalid_params()
     MQTTFixedBuffer_t networkBuffer;
     MQTTConnectInfo_t connectInfo;
 
+    /* Test NULL pConnectInfo. */
     mqttStatus = MQTT_SerializeConnect( NULL, NULL, remainingLength, &networkBuffer );
     TEST_ASSERT_EQUAL( mqttStatus, MQTTBadParameter );
 
-    mqttStatus = MQTT_SerializeConnect( &connectInfo, NULL, remainingLength, &networkBuffer );
+    /* Test NULL pBuffer. */
+    mqttStatus = MQTT_SerializeConnect( &connectInfo, NULL, remainingLength, NULL );
     TEST_ASSERT_EQUAL( mqttStatus, MQTTBadParameter );
+
+    /* Test connectPacketSize > pBuffer->size. */
+    networkBuffer.pBuffer = mqttBuffer;
+    networkBuffer.size = MQTT_PACKET_CONNECT_HEADER_SIZE - 1;
+    mqttStatus = MQTT_SerializeConnect( &connectInfo, NULL, remainingLength, &networkBuffer );
+    TEST_ASSERT_EQUAL( mqttStatus, MQTTNoMemory );
+}
+
+void test_Mqtt_SerializeConnect_happy_path()
+{
+    MQTTStatus_t mqttStatus = MQTTSuccess;
+    size_t remainingLength, packetSize;
+    MQTTFixedBuffer_t networkBuffer;
+    MQTTConnectInfo_t connectInfo;
+    MQTTPublishInfo_t willInfo;
+
+    networkBuffer.pBuffer = mqttBuffer;
+    networkBuffer.size = MQTT_TEST_BUFFER_LENGTH;
+
+    connectInfo.cleanSession = true;
+    connectInfo.pClientIdentifier = CLIENT_IDENTIFIER;
+    connectInfo.clientIdentifierLength = CLIENT_IDENTIFIER_LEN;
+    connectInfo.keepAliveSeconds = 0;
+    connectInfo.pUserName = MQTT_TEST_USERNAME;
+    connectInfo.userNameLength = MQTT_TEST_USERNAME_LEN;
+    connectInfo.pPassword = MQTT_TEST_PASSWORD;
+    connectInfo.passwordLength = MQTT_TEST_PASSWORD_LEN;
+
+    willInfo.pPayload = NULL;
+    willInfo.payloadLength = 0;
+    willInfo.pTopicName = MQTT_TEST_USERNAME;
+    willInfo.topicNameLength = MQTT_TEST_USERNAME_LEN;
+    willInfo.dup = true;
+    willInfo.qos = MQTTQoS1;
+    willInfo.retain = true;
+
+    /* We successfully call Mqtt_SerializeConnect until we have full coverage
+     * on the private method, serializeConnectPacket(...). */
+
+    /* Get MQTT connect packet size and remaining length. */
+    mqttStatus = MQTT_GetConnectPacketSize( &connectInfo,
+                                            &willInfo,
+                                            &remainingLength,
+                                            &packetSize );
+    TEST_ASSERT_EQUAL( mqttStatus, MQTTSuccess );
+    mqttStatus = MQTT_SerializeConnect( &connectInfo, &willInfo, remainingLength, &networkBuffer );
+    TEST_ASSERT_EQUAL( mqttStatus, MQTTSuccess );
+
+    /* Repeat with QoS2. */
+    willInfo.qos = MQTTQoS2;
+    mqttStatus = MQTT_GetConnectPacketSize( &connectInfo,
+                                            &willInfo,
+                                            &remainingLength,
+                                            &packetSize );
+    TEST_ASSERT_EQUAL( mqttStatus, MQTTSuccess );
+    mqttStatus = MQTT_SerializeConnect( &connectInfo, &willInfo, remainingLength, &networkBuffer );
+    TEST_ASSERT_EQUAL( mqttStatus, MQTTSuccess );
 }
