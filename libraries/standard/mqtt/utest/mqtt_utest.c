20f131b206841497133b40891890d5b688594291
Oscar Abrina <abrinao@amazon.com>
2020-05-18 16:02:45 -0700

    Add setup for testing handleIncomingPublish

--- libraries/standard/mqtt/utest/mqtt_utest.c
+++ libraries/standard/mqtt/utest/mqtt_utest.c
@@ -1,5 +1,7 @@
 #include "unity.h"
 
+#include "mock_mqtt_lightweight.h"
+
 /* Include paths for public enums, structures, and macros. */
 #include "mqtt.h"
 
@@ -7,6 +9,12 @@
  * @brief A valid starting packet ID per MQTT spec. Start from 1.
  */
 #define MQTT_NEXT_PACKET_ID_START    ( 1 )
+
+/**
+ * @brief Timeout for receiving a packet.
+ */
+#define MQTT_TIMEOUT_MS              ( 3000 )
+
 
 /* ============================   UNITY FIXTURES ============================ */
 
@@ -36,7 +44,7 @@ int suiteTearDown( int numFailures )
 /**
  * @brief Test that MQTT_Init is able to update the context object correctly.
  */
-void test_MQTT_Init_Happy_path( void )
+void test_MQTT_Init_happy_path( void )
 {
     MQTTStatus_t mqttStatus;
     MQTTContext_t context;
@@ -57,7 +65,7 @@ void test_MQTT_Init_Happy_path( void )
 /**
  * @brief Test that any NULL parameter causes MQTT_Init to return MQTTBadParameter.
  */
-void test_MQTT_Init_Invalid_params( void )
+void test_MQTT_Init_invalid_params( void )
 {
     MQTTStatus_t mqttStatus;
     MQTTContext_t context;
@@ -77,4 +85,88 @@ void test_MQTT_Init_Invalid_params( void )
 
     mqttStatus = MQTT_Init( &context, &transport, &callbacks, NULL );
     TEST_ASSERT_EQUAL( MQTTBadParameter, mqttStatus );
+}
+
+/* Mocked successful transport send. */
+static int32_t transportSendSuccess( MQTTNetworkContext_t pContext,
+                                     const void * pBuffer,
+                                     size_t bytesToWrite )
+{
+    ( void ) pContext;
+    ( void ) pBuffer;
+    return bytesToWrite;
+}
+
+/* Mocked successful transport read. */
+static int32_t transportRecvSuccess( MQTTNetworkContext_t pContext,
+                                     void * pBuffer,
+                                     size_t bytesToRead )
+{
+    ( void ) pContext;
+    ( void ) pBuffer;
+    return bytesToRead;
+}
+
+/**
+ * @brief The mocked timer query function provided to the MQTT context.
+ */
+static uint32_t getTime( void )
+{
+    return 0;
+}
+
+static void setupTransportInterface( MQTTTransportInterface_t * pTransport )
+{
+    pTransport->networkContext = 0;
+    pTransport->send = transportSendSuccess;
+    pTransport->recv = transportRecvSuccess;
+}
+
+static void setupCallbacks( MQTTApplicationCallbacks_t * pCallbacks )
+{
+    pCallbacks->getTime = getTime;
+}
+
+/**
+ * @brief Test that NULL pContext causes MQTT_ProcessLoop to return MQTTBadParameter.
+ */
+void test_MQTT_ProcessLoop_invalid_params( void )
+{
+    MQTT_ProcessLoop( NULL, MQTT_TIMEOUT_MS );
+}
+
+/* Mocked MQTT_GetIncomingPacketTypeAndLength callback that modifies pIncomingPacket
+ * to get full coverage on handleIncomingPublish. */
+static MQTTStatus_t modifyIncomingPacketPublish( MQTTTransportRecvFunc_t readFunc,
+                                                 MQTTNetworkContext_t networkContext,
+                                                 MQTTPacketInfo_t * pIncomingPacket,
+                                                 int cmock_num_calls )
+{
+    pIncomingPacket->type = MQTT_PACKET_TYPE_PUBLISH;
+}
+
+/**
+ * @brief Test coverage for handleIncomingPublish by using a CMock callback to
+ * modify incomingPacket.
+ */
+void test_MQTT_ProcessLoop_handleIncomingPublish( void )
+{
+    MQTTStatus_t mqttStatus;
+    MQTTContext_t context;
+    MQTTTransportInterface_t transport;
+    MQTTFixedBuffer_t networkBuffer;
+    MQTTApplicationCallbacks_t callbacks;
+    MQTTPacketInfo_t incomingPacket;
+
+    setupTransportInterface( &transport );
+    setupCallbacks( &callbacks );
+
+    mqttStatus = MQTT_Init( &context, &transport, &callbacks, &networkBuffer );
+    TEST_ASSERT_EQUAL( MQTTSuccess, mqttStatus );
+
+    MQTT_GetIncomingPacketTypeAndLength_AddCallback( modifyIncomingPacketPublish );
+
+    mqttStatus = MQTT_ProcessLoop( &context, MQTT_TIMEOUT_MS );
+
+    TEST_ASSERT_EQUAL( MQTTSuccess, mqttStatus );
 }
